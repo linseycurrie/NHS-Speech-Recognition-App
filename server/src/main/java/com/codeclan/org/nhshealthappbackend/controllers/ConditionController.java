de299258f6f11d4932d4a9da66859ec045a9951e
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-12-06 11:33:44 +0000

    finished controllers for all models

--- server/src/main/java/com/codeclan/org/nhshealthappbackend/controllers/ConditionController.java
+++ server/src/main/java/com/codeclan/org/nhshealthappbackend/controllers/ConditionController.java
@@ -1,14 +1,15 @@
 package com.codeclan.org.nhshealthappbackend.controllers;
 
 import com.codeclan.org.nhshealthappbackend.models.Condition;
+import com.codeclan.org.nhshealthappbackend.models.Favourite;
 import com.codeclan.org.nhshealthappbackend.models.User;
 import com.codeclan.org.nhshealthappbackend.repositories.ConditionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ConditionController {
@@ -16,10 +17,33 @@ public class ConditionController {
     @Autowired
     ConditionRepository conditionRepository;
 
-    @PostMapping(value = "/conditions")
-    public ResponseEntity<Condition> postCondition(@RequestBody Condition condition) {
-        conditionRepository.save(condition);
-        return new ResponseEntity<>(condition, HttpStatus.CREATED);
+    @GetMapping(value = "/conditions")
+    public ResponseEntity<List<Condition>> getAllConditions() {
+        return new ResponseEntity<>(conditionRepository.findAll(), HttpStatus.OK);
     }
+
+//    @GetMapping(value = "/conditions/{id}")
+//    public ResponseEntity getCondition(@PathVariable Long id) {
+//        return new ResponseEntity<>(conditionRepository.findById(id), HttpStatus.OK);
+//    }
+//
+//    @PostMapping(value = "/conditions")
+//    public ResponseEntity<Condition> postCondition(@RequestBody Condition condition) {
+//        conditionRepository.save(condition);
+//        return new ResponseEntity<>(condition, HttpStatus.CREATED);
+//    }
+//
+//    @PatchMapping(value = "/conditions/{id}")
+//    public ResponseEntity<Condition> updateCondition(@RequestBody Condition condition){
+//        conditionRepository.save(condition);
+//        return new ResponseEntity<>(condition, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping(value = "/conditions/{id}")
+//    public ResponseEntity<Condition> deleteCondition(@PathVariable Long id) {
+//        Condition found = conditionRepository.getOne(id);
+//        conditionRepository.delete(found);
+//        return new ResponseEntity<>(found, HttpStatus.OK);
+//    }
 
 }
--- server/src/main/java/com/codeclan/org/nhshealthappbackend/controllers/FavouriteController.java
+++ server/src/main/java/com/codeclan/org/nhshealthappbackend/controllers/FavouriteController.java
@@ -1,12 +1,47 @@
 package com.codeclan.org.nhshealthappbackend.controllers;
 
+import com.codeclan.org.nhshealthappbackend.models.Favourite;
+import com.codeclan.org.nhshealthappbackend.models.User;
 import com.codeclan.org.nhshealthappbackend.repositories.FavouriteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class FavouriteController {
 
     @Autowired
     FavouriteRepository favouriteRepository;
+
+//    @GetMapping(value = "/favourites")
+//    public ResponseEntity<List<Favourite>> getAllFavourites() {
+//        return new ResponseEntity<>(favouriteRepository.findAll(), HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/favourites/{id}")
+//    public ResponseEntity getFavourite(@PathVariable Long id) {
+//        return new ResponseEntity<>(favouriteRepository.findById(id), HttpStatus.OK);
+//    }
+//
+//    @PostMapping(value = "/")
+//    public ResponseEntity<Favourite> postFavourite(@RequestBody Favourite favourite) {
+//        favouriteRepository.save(favourite);
+//        return new ResponseEntity<>(favourite, HttpStatus.CREATED);
+//    }
+//
+//    @PatchMapping(value = "/favourites/{id}")
+//    public ResponseEntity<Favourite> updateFavourite(@RequestBody Favourite favourite){
+//        favouriteRepository.save(favourite);
+//        return new ResponseEntity<>(favourite, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping(value = "/favourites/{id}")
+//    public ResponseEntity<Favourite> deleteFavourite(@PathVariable Long id) {
+//        Favourite found = favouriteRepository.getOne(id);
+//        favouriteRepository.delete(found);
+//        return new ResponseEntity<>(found, HttpStatus.OK);
+//    }
 }
