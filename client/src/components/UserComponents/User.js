87e550eac9845382090581c027057bc9ea0e7c71
linseycurrie <linseycurrie@gmail.com>
2020-12-08 13:19:19 +0000

    users persisting from DB, need work on organisation

---
+++ client/src/components/UserComponents/User.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const User = ({user}) => {
+
+    if(!user){
+        return "Loading ..."
+    }
+
+    const url = "/user/" + user.id
+
+    return (
+        <>
+        <Link to = {url} >
+        <p>{user.firstName} {user.lastName} </p>
+        </Link>
+        </>
+    )
+}
+export default User;
--- client/src/containers/UserContainer.js
+++ client/src/containers/UserContainer.js
@@ -1,18 +1,74 @@
-import React from 'react';
-import UserFormComponent from '../components/UserFormComponent';
-import ReminderComponent from '../components/ReminderComponent';
+import React, {useState, useEffect} from 'react';
+import UserFormComponent from '../components/UserComponents/UserFormComponent';
+import UserDetailComponenet from '../components/UserComponents/UserDetailComponent';
+import UserListComponent from "../components/UserComponents/UserListComponent";
 import Request from '../helpers/request'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const UserContainer = () => {
+
+    const [allUsers, setAllUsers] = useState([]);
+   
+
+    const requestAllUsers = function(){
+        const request = new Request();
+        request.get("api/users")
+        .then((data) => {setAllUsers(data)})
+        
+    }
 
     const handlePost = function(user){
         const request = new Request();
         request.post("/api/", user)
-        .then(() => window.location = "/")
+        .then(() => window.location = "/user")
+    }
+
+    const findUserById = function(id){
+        return allUsers.find((user) => {
+            return user.id === parseInt(id);
+        })
+    }
+
+    const handleDelete = function(id){
+        const request = new Request();
+        const url = "/api/users/" + id
+        request.delete(url)
+        .then(() => window.location = "/user")
+      }
+    
+    useEffect(() => {
+        requestAllUsers()
+    }, [])
+
+
+    if(!allUsers){
+        return null
     }
 
     return(
-        <UserFormComponent onCreate={handlePost} />
+        <Router>
+        <>
+        <Switch>
+
+        
+        <Route exact path="/user/new" render={() => {
+            return <UserFormComponent onCreate={handlePost} />
+        }} />
+
+        <Route exact path="/user/:id" render={(props) => {
+            const id = props.match.params.id;
+            const user = findUserById(id);
+            return <UserDetailComponenet user={user} onDelete={handleDelete}/>
+        }} />
+
+        <Route render={() => {
+            return <UserListComponent allUsers={allUsers} />
+        }} />
+        
+        </ Switch>
+        </>
+        </Router>
     )
 }
 export default UserContainer;
