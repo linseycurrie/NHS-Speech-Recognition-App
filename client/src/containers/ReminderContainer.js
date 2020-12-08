dab3e888b5320642ce062b0ca4121d264396fe31
David Strain <strd79@hotmail.co.uk>
2020-12-08 13:38:28 +0000

    tweeks to css

--- client/src/containers/ReminderContainer.js
+++ client/src/containers/ReminderContainer.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import HeaderComponent from '../components/Header_Footer_elements/HeaderComponent';
 import ReminderComponent from '../components/ReminderComponent';
 import Request from '../helpers/request'
 
 const ReminderContainer = () => {
+
+    const headerCopy = "Your Reminders"
+
     const handlePost = function(reminder) {
         const request = new Request();
         request.post('/api/reminders', reminder)
     }
 
     return(
-        <ReminderComponent onCreate={handlePost} />
+        <>
+            <HeaderComponent headerCopy={headerCopy}/>
+            <ReminderComponent onCreate={handlePost} />
+        </>
     )
 }
 
