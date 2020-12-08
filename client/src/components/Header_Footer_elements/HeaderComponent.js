436ee6b18349af745990a692a24f4beaee5c8fe2
David Strain <strd79@hotmail.co.uk>
2020-12-08 19:29:55 +0000

    sorted further conflicts and re-pushing them

--- client/src/components/Header_Footer_elements/HeaderComponent.js
+++ client/src/components/Header_Footer_elements/HeaderComponent.js
@@ -6,11 +6,7 @@ const HeaderComponent = ({headerCopy, headerBanner}) => {
     <>
         <h1>{headerCopy}</h1>
         <img className="rainbow-seperator" src="/rainbow-seperator.png" alt=""></img>
-<<<<<<< HEAD
         <img className="header-image" src={headerBanner} alt=""></img>
-=======
-        <br></br>
->>>>>>> f49c60c6e3546e9551b009f4b9cb43706e0e0223
     </>
     )
 }
