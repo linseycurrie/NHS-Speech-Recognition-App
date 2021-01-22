5d5f5222daf15ac088408f4612577d7494ecce88
David Strain <strd79@hotmail.co.uk>
2021-01-22 13:43:27 +0000

    Copied over code from feature

--- client/src/containers/ServicesContainer.js
+++ client/src/containers/ServicesContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ServicesComponent from '../components/ServicesComponent';
 import HeaderComponent from '../components/Header_Footer_elements/HeaderComponent'
 import Request from '../helpers/request'
+import axios from 'axios';
 
 const ServicesContainer = () => {
 
@@ -11,19 +12,33 @@ const ServicesContainer = () => {
     const [serviceSearchResult, setServiceSearchResult] = useState(null);
 
     const handleSearchRequest = function(searchTerm) {
-        const request = new Request();
-        const body = {
-            "filter": "(OrganisationTypeID eq 'DEN') or (OrganisationTypeID eq 'GPB') or (OrganisationTypeID eq 'HOS')",
-            "top": 25,
+        // const request = new Request();
+        // const body = {
+        //     "filter": "(OrganisationTypeID eq 'DEN') or (OrganisationTypeID eq 'GPB') or (OrganisationTypeID eq 'HOS')",
+        //     "top": 25,
+        //     "skip": 0,
+        //     "count": true
+        // }
+        // const searchRequest = request.post("https://api.nhs.uk/service-search/search-postcode-or-place?api-version=1&search=lacock", body)
+        // .then((data) => {setServiceSearchResult(data)})
+
+        let body = {
+            "filter": "(OrganisationTypeID eq 'DEN')",
+            "top": 10,
             "skip": 0,
             "count": true
-        }
-        const searchRequest = request.post("https://api.nhs.uk/service-search/search-postcode-or-place?api-version=1&search=lacock", body)
-        .then((data) => {setServiceSearchResult(data)})
+        };
+         
+        const searchRequest = axios.post("https://api.nhs.uk/service-search/search-postcode-or-place?api-version=1&search=" + searchTerm, body, {
+            headers: {
+                "Content-Type": "application/json",
+                "subscription-key": "ca7e563eca174a80ad82eef61fc40776"
+            }
+        });
     }
 
     useEffect(() => {
-        handleSearchRequest();
+        handleSearchRequest("Manchester");
     }, [])
 
     return(
